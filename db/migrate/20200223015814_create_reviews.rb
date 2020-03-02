02c22747f6d07c86094161411359a69b97cbe4b3
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-03-02 15:31:27 +0800

    Singularize field

--- db/migrate/20200223015814_create_reviews.rb
+++ db/migrate/20200223015814_create_reviews.rb
@@ -1,8 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-      t.float :ratings
-      t.text :comments
+      t.float :rating
+      t.text :comment
       t.integer :customer_id
       t.integer :tasker_id
       t.integer :service_id
