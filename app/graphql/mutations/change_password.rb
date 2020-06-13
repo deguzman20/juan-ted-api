e28b682467e02dd17aab19511d33dc611c965cb1
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-13 09:05:33 +0800

    Modified resolver

--- app/graphql/mutations/change_password.rb
+++ app/graphql/mutations/change_password.rb
@@ -4,6 +4,7 @@ module Mutations
     description "Forgot Password"
 
     argument :id, Integer, required: true
+    argument :old_password, String, required: true
     argument :new_password, String, required: true
     argument :confirm_password, String, required: true
     argument :customer, Boolean, required: true
@@ -13,17 +14,17 @@ module Mutations
     field :email, String, null: false
 
     def resolve(_email: nil, **args)
-      customer_change_password = ChangePasswordService.call(args[:id], args[:new_password],
-                                                            args[:confirm_password], args[:customer])
-      if context[:current_user].present?
-        if customer_change_password.present?
-          { response: customer_change_password, status_code: 200 }
-        else
-          { response: "Password and confirm password doesn't match", status_code: 422 }
-        end
+      customer_change_password = ChangePasswordService.call(args[:id], args[:old_password], args[:new_password],
+                                                            args[:confirm_password], true)
+      # if context[:current_user].present?
+      if customer_change_password.present?
+        { response: customer_change_password, status_code: 200 }
       else
-        { response: "Unauthorized", status_code: 401 }
+        { response: "Password and confirm password doesn't match", status_code: 422 }
       end
+      # else
+      #   { response: "Unauthorized", status_code: 401 }
+      # end
     end
   end
 end
