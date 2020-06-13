56ece5ee02fc7c797d2a14390e31d0323c149148
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-13 09:06:01 +0800

    Modified third argument

--- app/graphql/mutations/create_todo.rb
+++ app/graphql/mutations/create_todo.rb
@@ -3,11 +3,12 @@ module Mutations
   class CreateTodo < BaseMutation
     description "Create Task Todo list"
     field :response, String, null: true
+    argument :customer_id, Integer, required: true
     argument :todo_description, String, required: true
 
     def resolve(** args)
       # if context[:current_customer].present?
-        response = CreateTodoService.call(args[:todo_description], context[:current_customer])
+        response = CreateTodoService.call(args[:todo_description], args[:customer_id])
         { response: response }
       # else
       #   { response: "Unauthorized" }
