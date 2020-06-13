41e96b9538f92c8546cf0ada37654ed679af6ffe
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-13 09:02:02 +0800

    Add todo ty[e

---
+++ app/graphql/types/todo_type.rb
@@ -0,0 +1,9 @@
+module Types
+  # query object for todo
+  class TodoType < BaseObject
+    description "Todo"
+    field :id, ID, null: false
+    field :todo_description, String, null: true
+    field :service, Types::ServiceType, null: true
+  end
+end 
