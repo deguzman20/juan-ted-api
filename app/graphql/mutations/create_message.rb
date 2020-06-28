4ab0ff52828fc6fc9dd57c479a66df7d69d3fafb
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-28 19:12:14 +0800

    Add own_by_customer field to message table and resolver

--- app/graphql/mutations/create_message.rb
+++ app/graphql/mutations/create_message.rb
@@ -3,11 +3,12 @@ module Mutations
     # argument :conversation_id, Integer, required: false
     argument :customer_id, Integer, required: true
     argument :tasker_id, Integer, required: true
+    argument :own_by_customer, Boolean, required: true
     argument :text, String, required: true
 
     field :message, Types::MessageType, null: false
 
-    def resolve(customer_id:, tasker_id:, text:)
+    def resolve(customer_id:, tasker_id:, text:, own_by_customer:)
       @convesation_id = nil
       @conversation = Conversation.where(customer_id: customer_id)
                                   .where(tasker_id: tasker_id)
@@ -20,7 +21,8 @@ module Mutations
       end
       
       repo = MessageRepository.new
-      @message = repo.create_in_room(conversation_id: @conversation_id, text: text)
+      @message = repo.create_in_room(conversation_id: @conversation_id, 
+                                      text: text, own_by_customer: own_by_customer)
       { message: @message }
     end
   end
