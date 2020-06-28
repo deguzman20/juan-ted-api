7ff520ab3d109413c3dd73eeb7176a5cab35b6c8
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-28 19:13:24 +0800

    Update top level documentation

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@
 # Table name: messages
 #
 #  id              :bigint           not null, primary key
+#  own_by_customer :boolean
 #  text            :text
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
