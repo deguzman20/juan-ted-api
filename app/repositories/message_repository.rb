01d65dae66b18306d37ec317543b78c11e3e38bf
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-28 19:13:59 +0800

    Add own_by_customer field

--- app/repositories/message_repository.rb
+++ app/repositories/message_repository.rb
@@ -14,8 +14,8 @@ class MessageRepository
       .reverse
   end
 
-  def create_in_room(conversation_id:, text:)
-    Message.create(conversation_id: conversation_id, text: text).tap do |message|
+  def create_in_room(conversation_id:, text:, own_by_customer:)
+    Message.create(conversation_id: conversation_id, text: text, own_by_customer: own_by_customer).tap do |message|
       JuanTedApiSchema.subscriptions.trigger(:message_added_to_conversation, { conversation_id: conversation_id }, message)
     end
   end
