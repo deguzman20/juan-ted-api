b89e9374f4fbec227e0c5d402cf0a86b04b30130
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-07-29 20:54:32 +0800

    Add condition

--- app/graphql/resolvers/query_type/past_tasker_resolver.rb
+++ app/graphql/resolvers/query_type/past_tasker_resolver.rb
@@ -8,8 +8,10 @@ module Resolvers
       type [Types::TransactionType], null: false
 
       def resolve(** args)
-        ::Transaction.where(customer_id: args[:customer_id])
-                     .where(done: true)  if args.present?
+        if args.present?
+          ::Transaction.where(customer_id: args[:customer_id])
+                       .where(done: true)
+        end
       end
     end
   end
