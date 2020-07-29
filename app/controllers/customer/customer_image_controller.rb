1b651b481eff4e1f8c0de1a51a71a175f78693e0
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-07-29 20:58:59 +0800

    Add controller for customer image

---
+++ app/controllers/customer/customer_image_controller.rb
@@ -0,0 +1,13 @@
+class Customer::CustomerImageController < ApplicationController
+  def update_customer_image
+    # @response = nil
+    # @customer = Customer.first
+    # @customer.image = params[:image]
+    # if @customer.save!
+    #   @response = { response: "Updated Successfuly", status_code: 200 }
+    # else
+    #   @response = { response: @customer.errors.full_messages.joins(","), status_code: 422 }
+    # end
+    # render json: .to_json
+  end
+end
