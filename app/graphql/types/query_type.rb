13f1aa044334af0a19e666b5c6e3c56a2811aff7
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-07-29 20:55:33 +0800

    Add field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,6 +9,7 @@ module Types
     field :service, resolver: Resolvers::QueryType::ServiceResolver
     field :tasker_by_geolocation, resolver: Resolvers::QueryType::TaskerByGeolocationResolver
     field :todo, resolver: Resolvers::QueryType::TodoResolver
+    field :transaction_service, resolver: Resolvers::QueryType::TransactionServiceResolver
     field :all_customers, resolver: Resolvers::QueryType::CustomersResolver
     field :all_service_type, resolver: Resolvers::QueryType::ServiceTypeResolver
     field :all_taskers, resolver: Resolvers::QueryType::TaskersResolver
@@ -18,6 +19,8 @@ module Types
     field :past_tasker_list, resolver: Resolvers::QueryType::PastTaskerResolver
     field :favorate_tasker_list, resolver: Resolvers::QueryType::FavorateTaskerResolver
     field :tasker_appointment_list, resolver: Resolvers::QueryType::TaskerAppointmentResolver
-    field :transaction_service, resolver: Resolvers::QueryType::TransactionServiceResolver
+    field :pending_transaction_service_info, resolver: Resolvers::QueryType::PendingTransactionServiceInfoResolver
+    field :pending_transaction_list, resolver: Resolvers::QueryType::PendingTransactionResolver
+    field :tasker_service_type_list, resolver: Resolvers::QueryType::TaskerServiceTypeResolver
   end
 end
