2f880bfbd511aad95a3516947c51bdd9ad052b98
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-08-07 11:55:12 +0800

    Add customer_id and tasker_id field for creating review

--- app/graphql/mutations/create_review.rb
+++ app/graphql/mutations/create_review.rb
@@ -4,13 +4,20 @@ module Mutations
     description "Create Review"
     argument :rating, Integer, required: true
     argument :comment, String, required: true
+    argument :customer_id, Integer, required: true
+    argument :tasker_id, Integer, required: true
     argument :service_type_id, Integer, required: true
 
     field :response, String, null: true
     field :status_code, Integer, null: false
 
     def resolve(** args)
-      review = Review.new(rating: args[:review].to_f, comment: args[:comment], service_type_id: args[:service_type_id])
+      review = Review.new(
+        rating: args[:review].to_f, 
+        comment: args[:comment],
+        customer_id: args[:customer_id],
+        tasker_id: args[:tasker_id],
+        service_type_id: args[:service_type_id])
       
       if review.save
         { response: "Review Created", status_code: 200 }
