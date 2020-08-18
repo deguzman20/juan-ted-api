ac82189db9f0d83eafa162f4466a1556e864061b
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-08-18 19:44:15 +0800

    Add argument for transaction_id and orm for updating transaction review

--- app/graphql/mutations/create_review.rb
+++ app/graphql/mutations/create_review.rb
@@ -7,7 +7,8 @@ module Mutations
     argument :customer_id, Integer, required: true
     argument :tasker_id, Integer, required: true
     argument :service_type_id, Integer, required: true
-
+    argument :transaction_id, Integer, required: true
+ 
     field :response, String, null: true
     field :status_code, Integer, null: false
 
@@ -17,10 +18,13 @@ module Mutations
         comment: args[:comment],
         customer_id: args[:customer_id],
         tasker_id: args[:tasker_id],
-        service_type_id: args[:service_type_id]
+        service_type_id: args[:service_type_id],
       )
 
-      if review.save
+      transaction = Transaction.find(args[:transaction_id])
+      transaction.review = true
+
+      if review.save && transaction.save
         { response: "Review Created", status_code: 200 }
       else
         { response: review.errors.full_messages.join(""), status_code: 422 }
