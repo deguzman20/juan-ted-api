0aea6aff998646278e3f2ca9bfdca82dfda3da38
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-08-29 19:51:52 +0800

    Remove line

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,7 +2,6 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-
     # TODO: remove me
     field :customer, resolver: Resolvers::QueryType::CustomerResolver
     field :tasker, resolver: Resolvers::QueryType::TaskerResolver
