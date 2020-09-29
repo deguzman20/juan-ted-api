cad1ef98f18a2f1143d08a4321fcae28e3b7f28b
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-09-29 10:53:11 +0800

    Add execute action

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,12 +1,30 @@
 class CheckoutController < ApplicationController
+  $customer_id = nil
+
   def redirect_to_paypal
+    $customer_id = params[:customer_id].to_i
     transaction_service = TransactionService.new
-    transaction = Transaction.find_by_customer_id(1)
+    transaction = Transaction.find_by_customer_id($customer_id)
     transaction_services = TransactionService.where(transaction_id: transaction.id)
     base_url = request.base_url
     redirect_to transaction_service.paypal_url(transaction_services, base_url)
   end
 
   def redirect
+  end
+
+  def execute
+    payment = PayPal::SDK::REST::Payment.find(params[:paymentId])
+    transactions = payment.links.find { |v| v.rel == 'approval_url' }
+    if payment.execute(payer_id: params[:PayerID])
+      subtotal = 0
+      transaction = Transaction.find_by_customer_id($customer_id.to_i).last
+      transaction.paid = true
+      if transaction.save
+        redirect_to transactions.href.to_s
+      end
+    else
+      payment.error # Error Hash
+    end
   end
 end
