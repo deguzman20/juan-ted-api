4e02c8da5a7628a5a00ea2aec555dfabfc87b12b
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-10-23 09:43:25 +0800

    Add last method

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -4,14 +4,13 @@ class CheckoutController < ApplicationController
   def redirect_to_paypal
     $customer_id = params[:customer_id].to_i
     transaction_service = TransactionService.new
-    transaction = Transaction.find_by_customer_id(params[:customer_id].to_i)
+    transaction = Transaction.where(customer_id: params[:customer_id].to_i).last
     transaction_services = TransactionService.where(transaction_id: transaction.id)
     base_url = request.base_url
     redirect_to transaction_service.paypal_url(transaction_services, base_url)
   end
 
-  def redirect
-  end
+  def redirect ;end
 
   def execute
     payment = PayPal::SDK::REST::Payment.find(params[:paymentId])
@@ -21,7 +20,7 @@ class CheckoutController < ApplicationController
       transaction = Transaction.where(customer_id: $customer_id.to_i).last
       transaction.paid = true
       if transaction.save
-        render json: 'Success'.to_json
+        
       end
     else
       payment.error # Error Hash
