f32dd5d92796f6811b20a331e2377ed60e0d474a
deguzman20 <alejandrogacumadeguzman@gmail.com>
2021-01-03 21:41:31 +0800

    Update controller

--- app/controllers/customer/upload_image_controller.rb
+++ app/controllers/customer/upload_image_controller.rb
@@ -1,5 +1,13 @@
 class Customer::UploadImageController < ApplicationController
   def customer_upload_image
-    render json: params.to_json
+    @message = nil
+    @upload_customer = Customer.find(1)
+    @upload_customer.image = params[:data]
+    if @upload_customer.save!
+      @message = 'Saved successfuly'
+    else
+      @message = 'Failed to saved'
+    end
+    render json: @message.to_json
   end
 end
