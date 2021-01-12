e8fbeebe8b4d2985dcf4f7e421845ff4afeda17d
deguzman20 <alejandrogacumadeguzman@gmail.com>
2021-01-12 08:42:48 +0800

    Added finding to DB

--- app/graphql/mutations/create_token.rb
+++ app/graphql/mutations/create_token.rb
@@ -9,17 +9,18 @@ module Mutations
     argument :exp_month, String, required: true
     argument :exp_year, String, required: true
     argument :cvc, String, required: true
+    argument :billing_address_id, Integer, required: true
 
-    argument :address_line_one, String, required: false
-    argument :address_line_two, String, required: false
-    argument :city, String, required: false
-    argument :state, String, required: false
-    argument :postal_code, String, required: false
-    argument :country, String, required: false
+    # argument :address_line_one, String, required: false
+    # argument :address_line_two, String, required: false
+    # argument :city, String, required: false
+    # argument :state, String, required: false
+    # argument :postal_code, String, required: false
+    # argument :country, String, required: false
 
-    argument :name, String, required: false
-    argument :email, String, required: false
-    argument :mobile_no, String, required: false
+    # argument :name, String, required: false
+    # argument :email, String, required: false
+    # argument :mobile_no, String, required: false
 
     field :response, String, null: true
     field :status_code, Integer, null: false
@@ -27,9 +28,7 @@ module Mutations
     
     def resolve(** args)
       token = CreateTokenService.call(args[:number], args[:exp_month], args[:exp_year], 
-                                      args[:cvc], args[:address_line_one], args[:address_line_two], 
-                                      args[:city], args[:state], args[:postal_code], args[:country], 
-                                      args[:name], args[:email], args[:mobile_no])
+                                      args[:cvc], args[:billing_address_id])
 
       obj = JSON.parse(token, object_class: OpenStruct)
       { response: obj.data.id, status_code: 200 }
--- app/services/create_token_service.rb
+++ app/services/create_token_service.rb
@@ -9,32 +9,32 @@ class CreateTokenService < ApplicationService
   attr_accessor :exp_year
   attr_accessor :cvc
 
-  attr_accessor :address_line_one
-  attr_accessor :address_line_two
-  attr_accessor :city
-  attr_accessor :state
-  attr_accessor :postal_code
-  attr_accessor :country
+  # attr_accessor :address_line_one
+  # attr_accessor :address_line_two
+  # attr_accessor :city
+  # attr_accessor :state
+  # attr_accessor :postal_code
+  # attr_accessor :country
 
-  attr_accessor :name
-  attr_accessor :email
-  attr_accessor :mobile_no
+  # attr_accessor :name
+  # attr_accessor :email
+  # attr_accessor :mobile_no
 
-  def initialize(number, exp_month, exp_year, cvc, address_line_one, address_line_two, 
-    city, state, postal_code, country, name, email, mobile_no)
+  def initialize(number, exp_month, exp_year, cvc, billing_address_id)
     @number = number
     @exp_month = exp_month
     @exp_year = exp_year
     @cvc = cvc
-    @address_line_one = address_line_one
-    @address_line_two = address_line_two
-    @city = city
-    @state = state
-    @postal_code = postal_code
-    @country = country
-    @name = name
-    @email = email
-    @mobile_no = mobile_no
+    @billing_address_id = billing_address_id
+    # @address_line_one = address_line_one
+    # @address_line_two = address_line_two
+    # @city = city
+    # @state = state
+    # @postal_code = postal_code
+    # @country = country
+    # @name = name
+    # @email = email
+    # @mobile_no = mobile_no
   end
 
   def call
@@ -44,6 +44,7 @@ class CreateTokenService < ApplicationService
   private
 
     def create_token
+      @billing_address = BillingAddress.find(@billing_address_id)
       url = URI("https://api.paymongo.com/v1/tokens")
 
       http = Net::HTTP.new(url.host, url.port)
@@ -60,16 +61,16 @@ class CreateTokenService < ApplicationService
               {\"billing\":
                 {\"address\":
                   {
-                    \"line1\":\"#{address_line_one}\",
-                    \"line2\":\"#{address_line_two}\",
-                    \"city\":\"#{city}\",
-                    \"state\":\"#{state}\",
-                    \"postal_code\":\"#{postal_code}\",
-                    \"country\":\"#{country}\"
+                    \"line1\":\"#{@billing_address.address_line_one}\",
+                    \"line2\":\"#{@billing_address.address_line_two}\",
+                    \"city\":\"#{@billing_address.city}\",
+                    \"state\":\"#{@billing_address.state}\",
+                    \"postal_code\":\"#{@billing_address.postal_code}\",
+                    \"country\":\"PH\"
                   },
-              \"name\":\"#{name}\",
-              \"email\":\"#{email}\",
-              \"phone\":\"#{mobile_no}\"
+              \"name\":\"#{@billing_address.first_name}\",
+              \"email\":\"#{@billing_address.email}\",
+              \"phone\":\"#{@billing_address.mobile_number}\"
               },
               \"number\":\"#{number}\",
               \"exp_month\":#{exp_month},
