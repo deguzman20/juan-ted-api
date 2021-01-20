4d64c8ad966195f579367d7beb97a61328e2e4ab
deguzman20 <alejandrogacumadeguzman@gmail.com>
2021-01-21 07:42:42 +0800

    Modified Timezone

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module JuanTedApi
     config.load_defaults 5.2
     config.autoload_paths << Rails.root.join("lib")
     ENV['RAILS_ADMIN_THEME'] = 'rollincode'
-    config.time_zone = 'Asia/Manila'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
