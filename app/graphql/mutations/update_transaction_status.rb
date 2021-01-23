8c71f7325016a0bd0f3b5d30b486a5efdbcb7472
deguzman20 <alejandrogacumadeguzman@gmail.com>
2021-01-23 12:13:55 +0800

    Added delete conversation when its completed

--- app/graphql/mutations/update_transaction_status.rb
+++ app/graphql/mutations/update_transaction_status.rb
@@ -10,7 +10,10 @@ module Mutations
     def resolve(** args)
       transaction = Transaction.find(args[:transaction_id]) if args[:transaction_id].present?
       transaction.approved = true
-      if transaction.save
+      conversation = Conversation.where(customer_id: transaction.customer_id)
+                                  .where(tasker_id: transaction.tasker_id)[0]
+
+      if transaction.save && conversation.delete
         { response: "Update was successfully", status_code: 200 }
       else
         { response: transaction.errors.full_messages.join(""), status_code: 422 }
