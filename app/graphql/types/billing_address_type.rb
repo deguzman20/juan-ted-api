efaa584246edfb54f046ec8464708746e4e76c41
deguzman20 <alejandrogacumadeguzman@gmail.com>
2021-05-02 11:17:53 +0800

    Add city to billing address type

--- app/graphql/types/billing_address_type.rb
+++ app/graphql/types/billing_address_type.rb
@@ -9,6 +9,7 @@ module Types
     field :state, String, null: true
     field :postal_code, String, null: true
     field :country, String, null: true
+    field :city, String, null: true
     field :email, String, null: true
     field :mobile_number, String, null: true
   end
